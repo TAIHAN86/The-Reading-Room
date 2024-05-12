3f0273d3341ecbebbf06546e8c7211647dcca702
taihan khane <taihankhan86@gmail.com>
2024-05-12 21:56:20 +0530

    banner added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Banner from './components/banner'
 
 function App() {
   return (
-  <><Navbar/> 
+  <>
+  <Navbar/> 
+  <Banner/>
   </>
   )
 }
